1b82fe0fff1ceef9b3c77869bd187c8180a26d72
Fernando Maldonado <f_maldonado6@hotmail.com>
2023-05-15 17:36:22 -0600

    Se termina seccion 1

---
+++ seccion_1/lib/models/notes.dart
@@ -0,0 +1,11 @@
+class Note {
+  String id;
+  String value;
+  DateTime dateCreated;
+
+  Note({
+    required this.id,
+    required this.value,
+    required this.dateCreated,
+  });
+}
---
+++ seccion_1/lib/screens/notes_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:seccion_1/di/container.dart';
+import 'package:seccion_1/models/notes.dart';
+import 'package:seccion_1/repositories/notes_repository.dart';
+import 'package:seccion_1/screens/add_notes.dart';
+
+class NotesScreen extends StatefulWidget {
+  const NotesScreen({super.key});
+
+  @override
+  State<NotesScreen> createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  final NotesRepository notesRepository = DIContainer.instance.notesRepository;
+  List<Note> notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getNotes();
+  }
+
+  getNotes() {
+    setState(() {
+      notes = notesRepository.notes;
+    });
+  }
+
+  addNote(String value) {
+    var note = Note(
+      dateCreated: DateTime.now(),
+      value: value,
+      id: DateTime.now().microsecondsSinceEpoch.toString(),
+    );
+
+    notesRepository.add(note);
+
+    getNotes();
+  }
+
+  removeNote(Note note) {
+    notesRepository.delete(note);
+    getNotes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Notas"),
+      ),
+      body: Builder(
+        builder: (context) {
+          if (notes.isEmpty) {
+            return const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.help,
+                    size: 40,
+                    color: Colors.grey,
+                  ),
+                  Text("No existen notas"),
+                ],
+              ),
+            );
+          }
+
+          return ListView.separated(
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              var item = notes[index];
+              return Dismissible(
+                key: Key(item.id),
+                onDismissed: (direction) {
+                  removeNote(item);
+                },
+                child: ListTile(
+                  leading: const Icon(Icons.note_alt_outlined),
+                  title: Text(item.value),
+                  subtitle: Text(
+                      "${item.dateCreated.day}/${item.dateCreated.month}/${item.dateCreated.year}"),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddNotesScreen(
+                onNoteAdded: (value) {
+                  addNote(value);
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
